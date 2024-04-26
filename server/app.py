405103557b7eefe1e25e8cab893a2476f4b019ed
Jasen <jasen.miyamoto@gmail.com>
2024-04-26 14:32:46 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        q = [message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+        return make_response(q)
+    elif request.method == 'POST':
+        new_message = Message(
+            body = request.json.get('body'),
+            username = request.json.get('username')
+        )
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        message_dict = new_message.to_dict()
+        return make_response(message_dict, 201)
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    q = Message.query.filter(Message.id == id).first()
+    
+    if request.method == 'GET':
+        return make_response(q.to_dict())
+    elif request.method == 'PATCH':
+        for attr in request.json:
+            setattr(q, attr, request.json.get(attr))
+        
+        db.session.add(q)
+        db.session.commit()
+
+        return make_response(q.to_dict())
+    elif request.method == 'DELETE':
+        db.session.delete(q)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."
+        }
+
+        return make_response(response_body)
 
 if __name__ == '__main__':
     app.run(port=5555)
